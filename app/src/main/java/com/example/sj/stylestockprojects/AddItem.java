ff9675143187012d77d8396c7bfafd4b3dd73c36
Real cheese <dbtjdwocjswo@gmail.com>
2018-07-09 16:28:31 +0900

    팝업 및 add 완료 firebase 에 추가 생성 및 저장 만 하면 완료

--- app/src/main/java/com/example/sj/stylestockprojects/AddItem.java
+++ app/src/main/java/com/example/sj/stylestockprojects/AddItem.java
@@ -10,6 +10,12 @@ public class AddItem extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        WindowManager.LayoutParams  layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags  = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount  = 0.7f;
+
         setContentView(R.layout.activity_add_item);
+
     }
 }
