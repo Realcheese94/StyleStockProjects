178ecd69f98d70a1e7cf35e97a78e8d7b78bafa7
motjsl00 <zldlsnlz753@naver.com>
2018-08-03 20:03:00 +0900

    제작

--- app/src/main/java/com/example/sj/stylestockprojects/Recommend_framework.java
+++ app/src/main/java/com/example/sj/stylestockprojects/Recommend_framework.java
@@ -12,6 +12,6 @@ public class Recommend_framework extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.recommend_layout,null);
-    }
+        return inflater.inflate(R.layout.recommend_layout, null);
+    }//제작
 }
